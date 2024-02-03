7aa38792195128b692345741bd8669dd5cea627a
davidcrair <115373655+davidcrair@users.noreply.github.com>
2024-02-03 17:16:58 -0500

    Created basic app to display user data

---
+++ src/components/DisplayUserData.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const DisplayUserData = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Spotify React</h1>
+            </header>
+        </div>
+    );
+}
