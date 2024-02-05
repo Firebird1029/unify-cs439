1779656af2596f10aec778573e94e0ee89f733d4
davidcrair <115373655+davidcrair@users.noreply.github.com>
2024-02-04 20:59:31 -0500

    Added user profile features and fixed some eslint issues

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable react/prop-types */
+
 "use client";
 
 import axios from "axios";
 import React, { useRef, useState } from "react";
+
+function removeDuplicates(strings) {
+  const uniqueStringsSet = new Set(strings);
+  const uniqueStringsArray = Array.from(uniqueStringsSet);
+  return uniqueStringsArray;
+}
 
 function SongPlayer({ song }) {
   const audioRef = useRef(null);
@@ -39,24 +47,11 @@ function SongPlayer({ song }) {
   );
 }
 
-function RenderUserProfile({ userProfile }) {
-  if (!userProfile) {
-    return <p>No user profile available.</p>;
-  }
-  return (
-    <div>
-      <p>Display Name: {userProfile.display_name}</p>
-      <p>User URL: {userProfile.external_urls.spotify}</p>
-      <p>URI: {userProfile.uri}</p>
-      <p>Total Followers: {userProfile.followers.total}</p>
-    </div>
-  );
-}
-
 function HomeContent() {
   const [token, setToken] = useState(null);
   const [userProfile, setUserProfile] = useState(null);
   const [topsongs, setTopSongs] = useState([]);
+  const [recommendedSong, setRecommendedSong] = useState([]);
   const [topartists, setTopArtists] = useState([]);
 
   const CLIENT_ID = "319f3f19b0794ac28b1df51ca946609c";
@@ -84,6 +79,30 @@ function HomeContent() {
     if (newToken) {
       setToken(newToken);
       window.localStorage.setItem("token", newToken);
+    }
+  };
+
+  function mode(arr) {
+    return arr
+      .sort(
+        (a, b) =>
+          arr.filter((v) => v === a).length - arr.filter((v) => v === b).length,
+      )
+      .pop();
+  }
+
+  const RenderUserProfile = () => {
+    if (!userProfile) {
+      return <p>No user profile available.</p>;
+    } else {
+      return (
+        <div>
+          <p>Display Name: {userProfile.display_name}</p>
+          <p>User URL: {userProfile.external_urls.spotify}</p>
+          <p>URI: {userProfile.uri}</p>
+          <p>Total Followers: {userProfile.followers.total}</p>
+        </div>
+      );
     }
   };
 
@@ -153,6 +172,75 @@ function HomeContent() {
     }
   };
 
+  const FetchTopItems = async (token, limit = 5, time_range = 'long_term', type = 'artists') => {
+    if (!token) {
+      console.error("Token not available. Please log in.");
+      return;
+    }
+    try {
+      const { data } = await axios.get(
+        `https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}&limit=${limit}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return data.items;
+    } catch (error) {
+      console.error(`Error fetching top ${type}:`, error);
+    }
+  };
+
+  const DisplayReccomendation = async () => {
+    console.log("reccomendation");
+    const topItems = await FetchTopItems(token, 5, "short_term", "tracks");
+    console.log(topItems);
+    console.log("flag");
+    let ids = [];
+    for (let i = 0; i < topItems.length; i += 1) {
+      console.log(topItems[i].id);
+      ids = ids.concat(topItems[i].id);
+    }
+    console.log(ids);
+    console.log(ids.join(", "));
+    const { data } = await axios.get(
+      `https://api.spotify.com/v1/recommendations?limit=5&seed_tracks=${ids.join(',')}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    console.log(data);
+    console.log(data.tracks[0]);
+    setRecommendedSong(data.tracks);
+    // console.log(SongPlayer(data.tracks[0]))
+    // return (<SongPlayer key={data.tracks[0].id} song={data.tracks[0]} />)
+  };
+
+  function DisplayUniquenessScore() {
+    let uniqueness = 0;
+    for (let i = 0; i < topartists.length; i++) {
+      // console.log(topartists[i].popularity);
+      uniqueness += topartists[i].popularity;
+    }
+    return (
+      <div>
+        Your Uniqueness Score:{" "}
+        {(100 - uniqueness / topartists.length).toFixed(0)}%
+      </div>
+    );
+  }
+
+  function DisplayTopGenre() {
+    let genres = [];
+    for (let i = 0; i < topartists.length; i++) {
+      genres = genres.concat(removeDuplicates(topartists[i].genres));
+    }
+    return <div>Your Top Genre: {mode(genres)}</div>;
+  }
+
   const RenderTopSongs = () => {
     return topsongs.map((song) => <SongPlayer key={song.id} song={song} />);
   };
@@ -160,6 +248,17 @@ function HomeContent() {
   const RenderTopArtists = () => {
     return topartists.map((artist) => <div key={artist.id}>{artist.name}</div>);
   };
+
+  function RenderReccomendation() {
+    return (
+      <div>
+        <h2>Recommended Songs:</h2>
+        {recommendedSong.map((song) => (
+          <SongPlayer key={song.id} song={song} />
+        ))}
+      </div>
+    );
+  }
 
   // Check for token in the URL hash when component mounts
   React.useEffect(() => {
@@ -183,6 +282,14 @@ function HomeContent() {
 
       {token && (
         <div className="mt-4">
+          <button type="button" className={`${buttonStyle}`} onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
+
+      {token && (
+        <div className="mt-4">
           <button
             type="button"
             className={`${buttonStyle}`}
@@ -192,6 +299,14 @@ function HomeContent() {
           </button>
         </div>
       )}
+
+      {token && userProfile != null && (
+        <div>User Profile:{RenderUserProfile()}</div>
+      )}
+
+      {token && userProfile != null && <div>{DisplayUniquenessScore()}</div>}
+
+      {token && userProfile != null && <div>{DisplayTopGenre()}</div>}
 
       {token && (
         <div className="mt-4">
@@ -203,6 +318,10 @@ function HomeContent() {
             Get Top Songs
           </button>
         </div>
+      )}
+
+      {token && topsongs.length > 0 && (
+        <div>Your Top Songs:{RenderTopSongs()}</div>
       )}
 
       {token && (
@@ -217,19 +336,25 @@ function HomeContent() {
         </div>
       )}
 
+      {token && topartists.length > 0 && (
+        <div>Your Top Artists:{RenderTopArtists()}</div>
+      )}
+
       {token && (
         <div className="mt-4">
-          <button type="button" className={`${buttonStyle}`} onClick={logout}>
-            Logout
+          <button
+            type="button"
+            className={`${buttonStyle}`}
+            onClick={DisplayReccomendation}
+          >
+            Display Reccomendation
           </button>
         </div>
       )}
 
-      {token && <div>User Profile:{RenderUserProfile({ userProfile })}</div>}
-
-      {token && <div>Your Top Songs:{RenderTopSongs()}</div>}
-
-      {token && <div>Your Top Artists:{RenderTopArtists()}</div>}
+      {token && recommendedSong.length > 0 && (
+        <div>{RenderReccomendation()}</div>
+      )}
     </div>
   );
 }
