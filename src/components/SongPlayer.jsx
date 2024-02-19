2bfb217088dc7252978ee4ffb1afe51bb32501ad
davidcrair <115373655+davidcrair@users.noreply.github.com>
2024-02-18 22:59:24 -0500

    Working spotify palette share feature

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -1,8 +1,27 @@
 import React, { useRef, useState, useEffect } from "react";
 
+import toImg from "react-svg-to-image";
+import * as htmlToImage from "html-to-image";
+import { Canvg } from "canvg";
+import GetSpotifyCode from "./SpotifyCodeGenerator";
+
 function SongPlayer({ song, token }) {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const addSongToQueue = () => {
+    // Use fetch for making HTTP requests
+    fetch(`http://localhost:5000/addSongToQueue?token=${token}&uri=${song.id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to add song to queue");
+        }
+        // Handle success if needed
+      })
+      .catch((error) => {
+        console.error("Error adding song to queue:", error);
+      });
+  };
 
   const togglePlay = () => {
     const audioElement = audioRef.current;
@@ -14,6 +33,55 @@ function SongPlayer({ song, token }) {
     }
 
     setIsPlaying(!isPlaying);
+  };
+
+  // Function to handle sharing
+  const shareSong = async () => {
+    console.log("sharing song");
+
+    // Use Web Share API to share the default image
+    const svgString = await GetSpotifyCode(song.external_urls.spotify);
+    console.log(svgString);
+
+    const img = new Image();
+
+    // Set the source of the image
+    img.src = `data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`;
+
+    // Wait for the image to load
+    img.onload = () => {
+      // Create a canvas element
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      // Draw the image on the canvas
+      canvas.getContext("2d")?.drawImage(img, 0, 0);
+
+      // Convert canvas to blob
+      canvas.toBlob((blob) => {
+        console.log(blob);
+
+        if (navigator.share) {
+          console.log("Web share API supported");
+          navigator
+            .share({
+              title: "Check out this song!",
+              text: `Listen to ${song.name}`,
+              url: song.external_urls.spotify,
+              files: [
+                new File([blob], "file.png", {
+                  type: blob.type,
+                }),
+              ],
+            })
+            .then(() => console.log("Shared successfully"))
+            .catch((error) => console.error("Error sharing:", error));
+        } else {
+          console.log("Web Share API not supported");
+        }
+      }, "image/png");
+    };
   };
 
   return (
@@ -32,6 +100,9 @@ function SongPlayer({ song, token }) {
       <audio ref={audioRef} src={song.preview_url}>
         <track kind="captions" />
       </audio>
+      <button type="button" onClick={shareSong}>
+        Share
+      </button>
     </div>
   );
 }
