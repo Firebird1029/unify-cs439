e8e89c77c7e8438af3333fd4fe3bf3682ad2432e
davidcrair <115373655+davidcrair@users.noreply.github.com>
2024-03-19 13:21:58 -0400

    Added user data object

--- src/app/userprofile/page.jsx
+++ src/app/userprofile/page.jsx
@@ -9,25 +9,7 @@ import ShareCassette from "@/components/svg-art/share_cassette";
 export default function UserProfilePage() {
   const [token, setToken] = useState(null);
   const [userProfile, setUserProfile] = useState(null);
-  const [topArtists, setTopArtists] = useState(null);
-  const [featuresData, setFeaturesData] = useState(null);
-
-  const fetchTopItems = async (type, timeRange, limit = 25) => {
-    if (!token) {
-      console.error("Token not available. Please log in.");
-      return null;
-    }
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/getTopItems?token=${token}&type=${type}&timeRange=${timeRange}&limit=${limit}`,
-      );
-      const data = await response.json();
-      return data.topItems;
-    } catch (error) {
-      console.error(`Error fetching top ${type}:`, error);
-      return error;
-    }
-  };
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const storedToken = window.localStorage.getItem("token");
@@ -54,18 +36,17 @@ export default function UserProfilePage() {
     }
   }, [token]);
 
+  // useEffect to get user data object
   useEffect(() => {
     // console.log("Token:", token);
     if (token) {
-      fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/getTopItems?token=${token}&type=artists`,
-      )
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/getUserData?token=${token}`)
         .then((res) => res.json())
         .then((data) => {
-          // console.log("top artists: ", data);
-          setTopArtists(data.topItems);
+          console.log("user data: ", data);
+          setUserData(data);
         })
-        .catch((error) => console.error("Error fetching top items:", error));
+        .catch((error) => console.error("Error fetching user data:", error));
     }
   }, [token]);
 
@@ -121,101 +102,23 @@ export default function UserProfilePage() {
       }, "image/png");
     };
   };
+
   const unify = () => {
     // console.log("unifying");
   };
 
-  const getAverageAudioFeatures = async () => {
-    const songs = await fetchTopItems("tracks", "long_term", 50);
-
-    if (!songs) {
-      console.error("failed to fetch top items");
-      return null;
-    }
-
-    // Join track IDs into comma-separated list for input
-    const trackIds = songs.map((track) => track.id).join(",");
-
-    // Calculate the sum of popularity from the songs array
-    // (popularity is in the songs array, whilst other data in audioFeatures)
-    const popularitySum = songs.reduce((acc, song) => acc + song.popularity, 0);
-
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/getAudioFeatures?token=${token}&ids=${trackIds}`,
-      );
-      const data = await response.json();
-      const audioFeatures = data.audio_features;
-
-      // Sum all the normalised (0-100) features within the 'Audio Features' data
-      const featuresSum = audioFeatures.reduce(
-        (acc, feature) => {
-          acc.acousticness += feature.acousticness;
-          acc.danceability += feature.danceability;
-          acc.energy += feature.energy;
-          acc.instrumentalness += feature.instrumentalness;
-          acc.speechiness += feature.speechiness;
-          acc.valence += feature.valence;
-          return acc;
-        },
-        {
-          acousticness: 0,
-          danceability: 0,
-          energy: 0,
-          instrumentalness: 0,
-          speechiness: 0,
-          valence: 0,
-        },
-      );
-
-      // Get the average at 0-100, not 0-1
-      const featuresAvg = Object.keys(featuresSum).reduce((acc, key) => {
-        acc[key] = (featuresSum[key] * 100) / audioFeatures.length;
-        return acc;
-      }, {});
-
-      // Add average popularity (already 0-100) to featuresAvg
-      featuresAvg.popularity = popularitySum / songs.length;
-
-      // Formatting to what is expected by Nivo
-      // Note that when supporting multiple users, format is {feature, value, value2...}
-      const formattedAvg = Object.keys(featuresAvg).map((key) => ({
-        feature: key,
-        value: featuresAvg[key],
-      }));
-
-      // console.log(formattedAvg);
-      return formattedAvg;
-    } catch (error) {
-      console.error("Error fetching audio features:", error);
-      return error;
-    }
-  };
-
-  // useEffect to get the data from getAverageAudioFeatures and push it to a state
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getAverageAudioFeatures();
-      setFeaturesData(data);
-      // console.log(data);
-    };
-
-    if (token) {
-      fetchData();
-    }
-  }, [token]);
-
   return (
     <div className="App">
-      {userProfile !== null && topArtists !== null ? (
+      {userProfile !== null && userData !== null ? (
         <UserContent
           displayName={userProfile.display_name}
-          userData={topArtists}
+          userData={userData}
           shareCassette={shareCassette}
           unify={unify}
-          featuresData={featuresData}
         />
-      ) : null}
+      ) : (
+        <div>loading...</div>
+      )}
     </div>
   );
 }
