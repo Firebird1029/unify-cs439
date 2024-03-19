d101c5cb9af01d6f3baee76551f7566ac6f78f80
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2024-03-19 01:06:11 -0400

    setup Supabase

---
+++ src/app/auth/signout/route.js
@@ -0,0 +1,23 @@
+// https://supabase.com/docs/guides/auth/server-side/nextjs
+
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+import createClient from "@/utils/supabase/server";
+
+export async function POST(req) {
+  const supabase = createClient();
+
+  // Check if a user's logged in
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    await supabase.auth.signOut();
+  }
+
+  revalidatePath("/", "layout");
+  return NextResponse.redirect(new URL("/login", req.url), {
+    status: 302,
+  });
+}
---
+++ src/utils/supabase/client.js
@@ -0,0 +1,10 @@
+// https://supabase.com/docs/guides/auth/server-side/nextjs
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export default function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  );
+}
