9fb0a629896c62f41da6b90733a3fcdded955967
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2024-04-01 22:45:34 -0400

    fix Next navigation in tests

--- __tests__/frontend.t.js
+++ __tests__/frontend.t.js
@@ -7,6 +7,17 @@ import { render } from "@testing-library/react";
 // Import the component to be tested
 import HomePage from "../src/app/Index"; // Adjust the import path to your actual file location
 
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
 // Jest snapshot test
 describe("Index Component", () => {
   it("matches the snapshot", () => {
