444e3e1d492d8a922ea358bb81a27938562ff1b6
davidcrair <115373655+davidcrair@users.noreply.github.com>
2024-04-14 20:33:58 -0400

    Log out bug fix

--- src/app/auth/signout/route.js
+++ src/app/auth/signout/route.js
@@ -22,7 +22,7 @@ export async function POST(req) {
   }
 
   revalidatePath("/", "layout");
-  return NextResponse.redirect(new URL("/login", req.url), {
+  return NextResponse.redirect(new URL("/", req.url), {
     status: 302,
   });
 }
