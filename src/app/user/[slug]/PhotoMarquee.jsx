a5ebc1c0a983eec7b35764f8b2a2dc0a3981262e
Ronald Cheng <ronaldcheng11@gmail.com>
2024-04-15 22:39:01 -0400

    finished stack and added picture gallery

---
+++ src/app/user/[slug]/PhotoMarquee.jsx
@@ -0,0 +1,31 @@
+export default function PhotoMarquee({ imageLinks }) {
+  return (
+    <div
+      style={{ width: `${imageLinks.length * 40}%` }}
+      className="relative flex overflow-x-hidden"
+    >
+      <div className="py-12 animate-marquee whitespace-nowrap">
+        {imageLinks.map((link) => {
+          return (
+            <img
+              className="mx-8 inline-block rounded-lg w-[10%] md:w-[7%] h-auto object-cover"
+              src={link}
+              alt="Artist"
+            />
+          );
+        })}
+      </div>
+      <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+        {imageLinks.map((link) => {
+          return (
+            <img
+              className="mx-4 inline-block rounded-lg w-[10%] h-auto object-cover"
+              src={link}
+              alt="Artist"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
